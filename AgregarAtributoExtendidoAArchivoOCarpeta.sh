f6ddd4c83f142a0fbe7dafb765502e378dc1e6a1
NiPeGun <nipegun@gmail.com>
2020-04-13 03:53:11 +0200

    Update AgregarAtributoExtendidoAArchivoOCarpeta.sh

--- AgregarAtributoExtendidoAArchivoOCarpeta.sh
+++ AgregarAtributoExtendidoAArchivoOCarpeta.sh
@@ -5,11 +5,22 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
+# -----------
+#
+#
+
 #
 # Usar AgregarAtributoExtendidoAArchivoOCarpeta Ruta TítuloAtributo Texto
 #
 # Ejemplo:
 # AgregarAtributoExtendidoAArchivoOCarpeta ~/Desktop/Prueba password "El perro salió de viaje"
 
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+echo ""
+echo -e "${ColorVerde}Agregando atributo extendido...${FinColor}"
+echo ""
 xattr -w $2 $3 $1
 
