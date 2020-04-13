8a7ea7e7983a0883ebf9edf0a2bebc7960042b5a
NiPeGun <nipegun@gmail.com>
2020-04-13 04:16:35 +0200

    Create SDKMan-Desinstalar.sh

---
+++ ÚnicaEjecución/SDKMan-Desinstalar.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#---------------------------------------------
+#  Script de NiPeGun para desinstalar SDKMan
+#---------------------------------------------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+echo ""
+echo -e "${ColorVerde}Desinstalando SDKMan...${FinColor}"
+echo ""
+rm -rf ~/.sdkman
+rm -rf ~/.bash_profile
+rm -rf ~/.zshrc
+
