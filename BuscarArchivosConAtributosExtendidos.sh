a42b087cfd67261a4acb390c4d69f23e448a63e0
NiPeGun <nipegun@gmail.com>
2020-04-13 03:48:28 +0200

    Update BuscarArchivosConAtributosExtendidos.sh

--- BuscarArchivosConAtributosExtendidos.sh
+++ BuscarArchivosConAtributosExtendidos.sh
@@ -5,5 +5,17 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
+#-------------------------------------------------------------------
+#  Script de NiPeGun para buscar archivos con atributos extendidos
+#-------------------------------------------------------------------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+echo ""
+echo -e "${ColorVerde}Borrando configuración bluetooth...${FinColor}"
+echo ""
+
 find $1 -xattr -exec xattr -v {} \;
 
