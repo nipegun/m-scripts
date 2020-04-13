47856db079060c18fbaf65bb8522e9f22065f28b
NiPeGun <nipegun@gmail.com>
2020-04-13 03:30:59 +0200

    Create Homebrew-Instalar.sh

---
+++ ÚnicaEjecución/Homebrew-Instalar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-----------------------------------------------------
+#  Script de NiPeGun para instalar Homebrew en macOS
+#-----------------------------------------------------
+
+CantArgsEsperados=1
+ArgsInsuficientes=65
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+echo ""
+echo -e "  $0 ${ColorVerde}Instalando hombrew...${FinColor}"
+echo ""
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
