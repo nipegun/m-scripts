212d72b54cd101db6c1d380f67334a3496c4f760
NiPeGun <nipegun@gmail.com>
2023-11-06 21:51:00 +0100

    Update Caches-Borrar-Mojave.sh

--- Caches-Borrar-Mojave.sh
+++ Caches-Borrar-Mojave.sh
@@ -5,38 +5,41 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------
-#  Script de NiPeGun para borrar todos los caches en MacOS 10.14.x (Mojave)
-#----------------------------------------------------------------------------
+# ----------
+# Script de NiPeGun para borrar todos los caches en MacOS 10.14.x (Mojave)
+#
+# Ejecución remota:
+#   curl -sL x | bash
+# ----------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+cColorRojo='\033[1;31m'
+cColorVerde='\033[1;32m'
+cFinColor='\033[0m'
 
 echo ""
-echo -e "${ColorVerde}  Borrando cachés de Mojave...${FinColor}"
+echo -e "${cColorVerde}  Borrando cachés de Mojave...${cFinColor}"
 echo ""
 
 echo ""
-echo "  Borrando el cache de DNS..."
+echo "    Borrando el cache de DNS..."
 echo ""
 sudo dscacheutil -flushcache
 sudo killall -HUP mDNSResponder
 
 echo ""
-echo "  Borrando todo el cache del usuario..."
+echo "    Borrando todo el cache del usuario..."
 echo ""
 #mv ~/Library/Caches/* ~/.Trash/
 find ~/Library/Caches/ -type f -print -exec rm -rf {} \; 2> /dev/null
 find ~/Library/Caches/ -type d -print -exec rm -rf {} \; 2> /dev/null
 
 echo ""
-echo "  Borrando todo el cache del sistema..."
+echo "    Borrando todo el cache del sistema..."
 echo ""
 #sudo rm -rf /System/Library/Caches
 sudo find /System/Library/Caches/ -type f -print -exec rm -rf {} \; 2> /dev/null
 sudo find /System/Library/Caches/ -type d -print -exec rm -rf {} \; 2> /dev/null
 
 echo ""
-echo "  Borrando el cache del kernel..."
+echo "    Borrando el cache del kernel..."
 echo ""
