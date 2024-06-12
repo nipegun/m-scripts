76c13cd6d92ac49d639815b415a060f3e0490a26
NiPeGun <nipegun@gmail.com>
2024-06-12 04:53:13 +0200

    Update Homebrew-Instalar.sh

--- SoftInst/Homebrew-Instalar.sh
+++ SoftInst/Homebrew-Instalar.sh
@@ -5,16 +5,201 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------
-#  Script de NiPeGun para instalar Homebrew en macOS
-#-----------------------------------------------------
+# ----------
+# Script de NiPeGun para instalar y configurar HomeBrew en macOS
+#
+# Ejecución remota:
+#  curl -sL https://raw.githubusercontent.com/nipegun/m-scripts/master/SoftInst/ParaCLI/HomeBrew-InstalarYConfigurar.sh | bash
+#
+# Ejecución remota sin caché:
+#  curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/m-scripts/master/SoftInst/ParaCLI/HomeBrew-InstalarYConfigurar.sh | bash
+#
+# Ejecución remota con parámetros:
+#  curl -sL https://raw.githubusercontent.com/nipegun/m-scripts/master/SoftInst/ParaCLI/HomeBrew-InstalarYConfigurar.sh | bash -s Parámetro1 Parámetro2
+# ----------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 
-echo ""
-echo -e "${ColorVerde}Instalando Homebrew...${FinColor}"
-echo ""
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# Determinar la versión de macOS
+
+  # Obtener la versión completa (con subversión)
+    cProductVersion=$(sw_vers -productVersion)
+  # Contar la cantidad de puntos
+    vCantPuntos=0
+    for (( i=0; i<${#cProductVersion}; i++ )); do
+      if [[ "${cProductVersion:$i:1}" == "." ]]; then
+        (( vCantPuntos++ ))
+      fi
+    done
+  # Guardar la versión sin subversión
+    if (( $vCantPuntos >= 2 )); then
+      # Elimina la última parte después del último punto
+      cVerSO="${cProductVersion%.*}"
+    else
+      # Usa toda la cadena
+      cVerSO="$cProductVersion"
+    fi
+
+# Elegir que hacer según la versión de macOS
+
+  if [ $cVerSO == "15" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 15 (Sequoia)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 15 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "14" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 14 (Sonoma)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 14 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "13" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 13 (Ventura)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 13 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "12" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 12 (Monterey)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 12 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "11" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 11 (Big Sur)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 11 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10.15" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 10.15 (Catalina)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 10.15 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10.14" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 10.14 (Mojave)...${cFinColor}"
+    echo ""
+
+    # Ejecutar el script de instalación
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Reparar permisos
+      sudo chmod 777 /usr/local/share/man/es/man1
+
+  elif [ $cVerSO == "10.13" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 10.13 (High Sierra)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 10.13 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10.12" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 10.12 (Sierra)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 10.12 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10.11" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 10.11 (El Capitan)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 10.11 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10.10" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 10.10 (Yosemite)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 10.10 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10.9" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 10.9 (Mavericks)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 10.9 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10.8" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 10.8 (Mountain Lion)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 10.8 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10.7" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 10.7 (Lion)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 10.7 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10.6" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para macOS 10.6 (Snow Leopard)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para macOS 10.6 todavía no preparados. Prueba ejecutarlo en otra versión de macOS.${cFinColor}"
+    echo ""
+
+  fi
 
